0f1792bdfe504a19788794f0c83337aad7343e66
Anastase Tuyizere <42033331+TuyizeeAnastase@users.noreply.github.com>
2023-03-07 12:23:21 +0200

    add-cross-origin

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import routes from "./routers";
 import dotenv from "dotenv"
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = 3500;
+
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 const server = app.listen(PORT, console.log(`Server Listening on ${PORT} `));
